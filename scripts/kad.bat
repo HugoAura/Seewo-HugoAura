502da5ba9a8d112018fd7202372d74406f96f246
Minoricew <154642983+Minoricew@users.noreply.github.com>
2025-04-19 00:22:47 +0800

    [Chore] Upload kad.bat

---
+++ scripts/kad.bat
@@ -0,0 +1,5 @@
+taskkill /f /im SeewoServiceAssistant.exe
+taskkill /f /im SeewoServiceAssistant.exe
+taskkill /f /im SeewoServiceAssistant.exe
+del /f /s /q .\app.asar
+move .\app-unpacked .\app.asar
